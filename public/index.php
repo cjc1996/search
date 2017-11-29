c84f456e30cb478ee99a0e3e82abd7108e8202f9
cjc1996 <cchambers102@gmail.com>
2017-11-29 13:01:18 +0000

    Commit 3

--- public/index.php
+++ public/index.php
@@ -8,6 +8,27 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php 
+        $server = 'v.je';
+        $username = 'student';
+        $password = 'student';
+        $schema = 'csy2028';
+
+        $pdo = new PDO('mysql:dbname=' . $schema . ';host=' . $server, $username, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+        $results = $pdo->query('SELECT * FROM person');
+    ?>
+    <header>Header</header>
+    <nav>Nav</nav>
+    <main>
+        <?php 
+            foreach($results as $row) 
+            {
+                echo '<ul>';
+                echo '<li>' . $row['forename'] . '</li>';
+                echo '</ul>';
+            }
+        ?>
+    </main>
+    <footer>Footer</footer>
 </body>
 </html>
